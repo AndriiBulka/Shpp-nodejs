2264cfe622392dad8727fa07e4fa12db8beb8c05
Andrii Bulka <bulkaandrii@gmail.com>
2023-01-30 23:51:40 +0200

    Add http response to task

--- 1-basics/1.2-http-and-web/go2.js
+++ 1-basics/1.2-http-and-web/go2.js
@@ -24,7 +24,41 @@ function readHttpLikeInput() {
 
 let contents = readHttpLikeInput();
 
-// вот эту функцию собственно надо написать
+function outputHttpResponse(statusCode, statusMessage, headers, body) {
+  const response = `
+HTTP/1.1 ${statusCode} ${statusMessage}
+${Object.keys(headers)
+  .reduce((acc, item) =>
+      acc + item + " : " + headers[item] + "\n", "")}
+        
+${body}`;
+ console.log(response);
+}
+
+function processHttpRequest($method, $uri, $headers, $body) {
+  const status = {
+    "200": "OK",
+    "400": "Bad Request",
+    "404": "Not Found"
+};
+const statusCode =  $method !== "GET" ? "400" : !$uri.startsWith("/sum")? "404" : !$uri.includes("?nums=")? "400" : "200";
+const statusMessage = status[statusCode];
+const sumOfnum = statusCode === "200" 
+    ? $uri.match(/[^(nums=)](,?\d)+/g)[0]
+    .split(",")
+    .reduce((accumulator, current) => +accumulator + +current, 0):undefined;
+const body = sumOfnum || status[statusCode];
+
+const headers = {
+    "Date": new Date(),
+    "Server": "Apache / 2.2.14 (Win32)",
+    "Connection": "Closed",
+    "Content-Type": "text/html; charset=utf-8",
+    "Content-Length": body.toString().length,
+};
+outputHttpResponse(statusCode, statusMessage, headers, body);
+}
+
 function parseTcpStringAsHttpRequest(string) {
   let method = string.match(/\w+/)[0].trim();
   let uri = string.match(/\/[^\s]+/)[0].trim();
@@ -38,17 +72,15 @@ function parseTcpStringAsHttpRequest(string) {
     acc[head] = value;
     return acc;
   }, {});
-  let body = string
-    .match(/^(.*=[^\s,]*&?)+$/gm)
-    .join()
-    .trim();
+  let body = string.match(/^(.*=[^\s,]*&?)+$/gm);
+  body = body ? body.join().trim() : undefined;  
   return {
-    method,
-    uri,
-    headers,
-    body,
+      method, uri, headers, body,   
   };
 }
-
+const testRequest = `
+GET /sum?nums=1,2,3 HTTP/1.1
+Host: student.shpp.me`;
 http = parseTcpStringAsHttpRequest(contents);
-console.log(JSON.stringify(http, undefined, 2));
+processHttpRequest(http.method, http.uri, http.headers, http.body);
+
