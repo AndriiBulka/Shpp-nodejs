d261bddc568dbcfb79ea485215b2958adfd671ba
Andrii Bulka <bulkaandrii@gmail.com>
2023-02-01 21:27:28 +0200

    Add http task forms and html

--- 1-basics/1.2-http-and-web/go2.js
+++ 1-basics/1.2-http-and-web/go2.js
@@ -23,42 +23,91 @@ function readHttpLikeInput() {
 }
 
 let contents = readHttpLikeInput();
+// let contents =`
+// POST /api/checkLoginAndPassword HTTP/1.1
+// Accept: */*
+// Content-Type: application/x-www-form-urlencoded
+// User-Agent: Mozilla/4.0
+// Content-Length: 35
 
+// login=student&password=12345
+// `
+
+// task http 1.2.3 
 function outputHttpResponse(statusCode, statusMessage, headers, body) {
   const response = `
 HTTP/1.1 ${statusCode} ${statusMessage}
 ${Object.keys(headers)
   .reduce((acc, item) =>
       acc + item + " : " + headers[item] + "\n", "")}
-        
 ${body}`;
  console.log(response);
 }
 
+// task  http 1.2.4
 function processHttpRequest($method, $uri, $headers, $body) {
   const status = {
     "200": "OK",
+    "204": "",
     "400": "Bad Request",
-    "404": "Not Found"
+    "404": "Not Found",
+    "500": "Internal Server Error",
 };
-const statusCode =  $method !== "GET" ? "400" : !$uri.startsWith("/sum")? "404" : !$uri.includes("?nums=")? "400" : "200";
-const statusMessage = status[statusCode];
-const sumOfnum = statusCode === "200" 
-    ? $uri.match(/[^(nums=)](,?\d)+/g)[0]
-    .split(",")
-    .reduce((accumulator, current) => +accumulator + +current, 0):undefined;
-const body = sumOfnum || status[statusCode];
-
-const headers = {
-    "Date": new Date(),
-    "Server": "Apache / 2.2.14 (Win32)",
+  const isValidUri = $uri === "/api/checkLoginAndPassword";
+  const isValidContentType = $headers["Content-Type"] === "application/x-www-form-urlencoded";
+  const isValidMathod = $method === "POST";
+  let statusCode = !isValidMathod ? "400" : !isValidContentType || !isValidUri ? "404" : "200"
+  const login = $body.match(/login=(\w+)?/)[1].trim()
+  const password = $body.match(/.+password=(\w+)?/)[1].trim()
+  let data;
+  try{
+   data = require("fs").readFileSync("passwords.txt")
+  .toString()
+  .split("\n")
+  .map((user) => {
+    let data = user.split(":")
+    return{ login: data[0].trim(),password: data[1].trim()};
+})
+  }catch(err){
+     statusCode = "500"
+  }
+  const statusMessage = status[statusCode];
+  const isValidUSer = statusCode !=="500"? data.find((user) => user.login === login && user.password === password) : undefined;
+  let  body = isValidUSer && statusCode === "200" ? `<h1 style="color:green">FOUND</h1>` : statusMessage;
+  const headers = {
+    "Server": "Apache/2.2.14 (Win32)",
+    "Content-Length": body.length,
     "Connection": "Closed",
     "Content-Type": "text/html; charset=utf-8",
-    "Content-Length": body.toString().length,
 };
 outputHttpResponse(statusCode, statusMessage, headers, body);
 }
 
+// task  http 1.2.3
+// function processHttpRequest($method, $uri, $headers, $body) {
+//   const status = {
+//     "200": "OK",
+//     "400": "Bad Request",
+//     "404": "Not Found"
+// };
+// const statusCode =  $method !== "GET" ? "400" : !$uri.startsWith("/sum")? "404" : !$uri.includes("?nums=")? "400" : "200";
+// const statusMessage = status[statusCode];
+// const sumOfnum = statusCode === "200" 
+//     ? $uri.match(/[^(nums=)](,?\d)+/g)[0]
+//     .split(",")
+//     .reduce((accumulator, current) => +accumulator + +current, 0):undefined;
+// const body = sumOfnum || status[statusCode];
+
+// const headers = {
+//     "Date": new Date(),
+//     "Server": "Apache / 2.2.14 (Win32)",
+//     "Connection": "Closed",
+//     "Content-Type": "text/html; charset=utf-8",
+//     "Content-Length": body.toString().length,
+// };
+// outputHttpResponse(statusCode, statusMessage, headers, body);
+// }
+// task http 1.2.2
 function parseTcpStringAsHttpRequest(string) {
   let method = string.match(/\w+/)[0].trim();
   let uri = string.match(/\/[^\s]+/)[0].trim();
@@ -78,9 +127,7 @@ function parseTcpStringAsHttpRequest(string) {
       method, uri, headers, body,   
   };
 }
-const testRequest = `
-GET /sum?nums=1,2,3 HTTP/1.1
-Host: student.shpp.me`;
+
 http = parseTcpStringAsHttpRequest(contents);
 processHttpRequest(http.method, http.uri, http.headers, http.body);
 
