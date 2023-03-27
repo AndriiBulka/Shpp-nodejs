e419ea5414a5f65c284e4b7c451adc716a2950c0
Andrii Bulka <106112143+AndriiBulka@users.noreply.github.com>
2023-03-27 12:08:01 +0300

    Delete 2-primitive servers/2.4 todo-app-rest-mongodb/dist directory

--- 2-primitive servers/2.4 todo-app-rest-mongodb/dist/database/connectMongoDB.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongodb_1 = require("mongodb");
-const username = encodeURIComponent("admin");
-const password = encodeURIComponent("admin1234");
-const DB_URL = `mongodb+srv://${username}:${password}@cluster0.2qkuxkp.mongodb.net/app_db?retryWrites=true&w=majority`;
-const client = new mongodb_1.MongoClient(DB_URL);
-exports.default = client;
